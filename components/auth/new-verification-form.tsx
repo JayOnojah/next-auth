bd886244f7bd569a17d9f53d4d30bab859f52a8d
Jeremiah Onojah <jayonojah@gmail.com>
2024-05-14 18:00:44 +0100

    New oVerification Form Completion

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,5 +1,33 @@
 'use client';
 
+import { useCallback, useEffect } from 'react';
+import { BeatLoader } from 'react-spinners';
+import { useSearchParams } from 'next/navigation';
+
+import { CardWrapper } from '@/components/auth/card-wrapper';
+
 export const NewVerificationForm = () => {
-  return <div>New Verification Form</div>;
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get('token');
+
+  const onSubmit = useCallback(() => {
+    console.log(token);
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to Login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center w-full justify-center">
+        <BeatLoader />
+      </div>
+    </CardWrapper>
+  );
 };
