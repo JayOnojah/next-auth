4e04a7fc93f9f3195e2e36aee2edf829c957c322
Jeremiah Onojah <jayonojah@gmail.com>
2024-05-14 11:21:51 +0100

    Resend (Sending Emails) Completion

--- actions/register.ts
+++ actions/register.ts
@@ -7,6 +7,7 @@ import { db } from '@/lib/db';
 import { RegisterSchema } from '@/schemas';
 import { getUserByEmail } from '@/data/user';
 import { generateVerificationToken } from '@/lib/tokens';
+import { sendVerificationEmail } from '@/lib/mail';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -33,7 +34,7 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   });
 
   const verificationToken = await generateVerificationToken(email);
-  // TODO: Send verification token email;
+  await sendVerificationEmail(verificationToken.email, verificationToken.token);
 
-  return { success: 'Confirmation email was sent successfully!' };
+  return { success: 'A confirmation link was sent to your email.' };
 };
