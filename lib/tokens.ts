e0cd6c63d46365401231a989e411037ea9476b6c
Jeremiah Onojah <jayonojah@gmail.com>
2024-05-18 00:55:32 +0100

    Logout Test

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -57,7 +57,7 @@ export const generatePasswordResetToken = async (email: string) => {
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
-  const expires = new Date(new Date().getTime() + 3600 * 1000);
+  const expires = new Date(new Date().getTime() + 10 * 60 * 1000);
 
   const existingToken = await getVerificationTokenByEmail(email);
 
