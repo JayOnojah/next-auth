a01fcee1db670b62cd20557207ae32cf9b52bba0
Jeremiah <jayonojah@gmail.com>
2024-06-03 12:02:42 +0100

    User Button Completion

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -2,7 +2,9 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@/components/auth/user-button";
 
 export const Navbar = () => {
   const pathname = usePathname();
@@ -17,11 +19,19 @@ export const Navbar = () => {
         </Button>
         <Button
           asChild
+          variant={pathname === "/client" ? "default" : "outline"}>
+          <Link href="/client">Client</Link>
+        </Button>
+        <Button asChild variant={pathname === "/admin" ? "default" : "outline"}>
+          <Link href="/admin">Admin</Link>
+        </Button>
+        <Button
+          asChild
           variant={pathname === "/settings" ? "default" : "outline"}>
           <Link href="/settings">Settings</Link>
         </Button>
       </div>
-      <p>User Button</p>
+      <UserButton />
     </nav>
   );
 };
