2114587cd85cd01d8ad44accfc1fd012e1575b51
Jeremiah <jayonojah@gmail.com>
2024-06-04 23:25:39 +0100

    Settings Page Completion

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,11 +1,14 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 
 import { db } from "@/lib/db";
 import { SettingsSchema } from "@/schemas";
-import { getUserById } from "@/data/user";
+import { generateVerificationToken } from "@/lib/tokens";
+import { getUserById, getUserByEmail } from "@/data/user";
 import { currentUser } from "@/lib/auth";
+import { sendVerificationEmail } from "@/lib/mail";
 
 export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
@@ -20,6 +23,45 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     return { error: "Unauthorized" };
   }
 
+  if (user.isOAuth) {
+    values.email = undefined;
+    values.password = undefined;
+    values.newPassword = undefined;
+    values.isTwoFactorEnabled = undefined;
+  }
+
+  if (values.email && values.email !== user.email) {
+    const existingUser = await getUserByEmail(values.email);
+
+    if (existingUser && existingUser.id !== user.id) {
+      return { error: "The email address provided is already in use." };
+    }
+
+    const verificationToken = await generateVerificationToken(values.email);
+
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
+    return { success: "A verification link was sent to your email." };
+  }
+
+  if (values.password && values.newPassword && dbUser.password) {
+    const passwordsMatch = await bcrypt.compare(
+      values.password,
+      dbUser.password
+    );
+
+    if (!passwordsMatch) {
+      return { error: "You have provided an invalid password." };
+    }
+
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
+    values.password = hashedPassword;
+    values.newPassword = undefined;
+  }
+
   await db.user.update({
     where: { id: dbUser.id },
     data: {
@@ -27,5 +69,5 @@ export const settings = async (values: z.infer<typeof SettingsSchema>) => {
     },
   });
 
-  return { success: "Settings Updated!" };
+  return { success: "Your profile data was updated successfully." };
 };
